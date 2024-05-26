857d0566a10e9093efd28302edb7bf5614ebfe3e
PRATEEK MISHRA <153118081+PrateekMishra-1@users.noreply.github.com>
2024-05-26 10:46:52 +0530

    Update usercontroller.js (III)

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,26 +1,24 @@
 import { User } from "../models/userSchema.js";
-import bcryptjs from "bcryptjs"
-import jwt from "jsonwebtoken"
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const Register = async (req, res) => {
     try {
         const { name, username, email, password } = req.body;
-        //basic validation
-        if (!name || !email || !username || !password) {
+        // basic validation
+        if (!name || !username || !email || !password) {
             return res.status(401).json({
-                message: "All fields are required",
+                message: "All fields are required.",
                 success: false
             })
         }
-
         const user = await User.findOne({ email });
         if (user) {
             return res.status(401).json({
-                message: "User already exists.",
+                message: "User already exist.",
                 success: false
             })
         }
-
         const hashedPassword = await bcryptjs.hash(password, 16);
 
         await User.create({
@@ -28,10 +26,9 @@ export const Register = async (req, res) => {
             username,
             email,
             password: hashedPassword
-        })
-
+        });
         return res.status(201).json({
-            message: "Account created successfully",
+            message: "Account created successfully.",
             success: true
         })
 
@@ -39,25 +36,21 @@ export const Register = async (req, res) => {
         console.log(error);
 
         return res.status(500).json({
-            message : "Something went wrong",
-            success : false,
-            data : error
+            message: "Error.... Register",
+            success: false
         })
+
     }
 }
-
-
 export const Login = async (req, res) => {
-
     try {
-        const { email, password } = req.body
+        const { email, password } = req.body;
         if (!email || !password) {
             return res.status(401).json({
-                message: "All fields are required",
+                message: "All fields are required.",
                 success: false
             })
-        }
-
+        };
         const user = await User.findOne({ email });
         if (!user) {
             return res.status(401).json({
@@ -65,79 +58,72 @@ export const Login = async (req, res) => {
                 success: false
             })
         }
-
-        const isMatch = await bcryptjs.compare(password, user.password);   // to match ki entered password wai hai na jo DB me stored hai
+        const isMatch = await bcryptjs.compare(password, user.password);
         if (!isMatch) {
             return res.status(401).json({
-                message: "Incorrect email or password",
+                message: "Incorect email or password",
                 success: false
-            })
+            });
         }
-        //Match ho gya password
         const tokenData = {
             userId: user._id
         }
-        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET, { expiresIn: "1d" })
-
+        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET, { expiresIn: "1d" });
         return res.status(201).cookie("token", token, { expiresIn: "1d", httpOnly: true }).json({
             message: `Welcome back ${user.name}`,
             user,
             success: true
         })
-
     } catch (error) {
         console.log(error);
-        
-        return res.status(500).json({
-            message : "Something went wrong",
-            success : false,
-    })
-}
 
+        return res.status(500).json({
+            message: "Error.... Login",
+            success: false
+        })
+
+    }
+}
 export const logout = (req, res) => {
     return res.cookie("token", "", { expiresIn: new Date(Date.now()) }).json({
-        message: "User Loggedout successfully",
+        message: "user logged out successfully.",
         success: true
     })
 }
 
-export const bookmarks = async (req, res) => {
+export const bookmark = async (req, res) => {
     try {
         const loggedInUserId = req.body.id;
         const tweetId = req.params.id;
-
         const user = await User.findById(loggedInUserId);
         if (user.bookmarks.includes(tweetId)) {
-            //remove
+            // remove
             await User.findByIdAndUpdate(loggedInUserId, { $pull: { bookmarks: tweetId } });
             return res.status(200).json({
                 message: "Removed from bookmarks."
-            })
-        }
-        else {
-            //bookmark
+            });
+        } else {
+            // bookmark
             await User.findByIdAndUpdate(loggedInUserId, { $push: { bookmarks: tweetId } });
             return res.status(200).json({
-                message: "Added to bookmarks."
-            })
+                message: "Saved to bookmarks."
+            });
         }
     } catch (error) {
         console.log(error);
     }
-}
-
+};
 export const getMyProfile = async (req, res) => {
     try {
         const id = req.params.id;
-        const user = await User.findById(id).select("-password"); // to remove password in response
-
+        const user = await User.findById(id).select("-password");
         return res.status(200).json({
-            user
+            user,
         })
     } catch (error) {
         console.log(error);
     }
-}
+};
 
 export const getOtherUsers = async (req, res) => {
     try {
@@ -145,10 +131,9 @@ export const getOtherUsers = async (req, res) => {
         const otherUsers = await User.find({ _id: { $ne: id } }).select("-password");
         if (!otherUsers) {
             return res.status(401).json({
-                message: "Currently no users"
+                message: "Currently do not have any users."
             })
-        }
-
+        };
         return res.status(200).json({
             otherUsers
         })
@@ -161,35 +146,30 @@ export const follow = async (req, res) => {
     try {
         const loggedInUserId = req.body.id;
         const userId = req.params.id;
-
-        const loggedInUser = await User.findById(loggedInUserId); //patel
-        const user = await User.findById(userId);  //keshav
-
+        const loggedInUser = await User.findById(loggedInUserId);//patel
+        const user = await User.findById(userId);//keshav
         if (!user.followers.includes(loggedInUserId)) {
             await user.updateOne({ $push: { followers: loggedInUserId } });
             await loggedInUser.updateOne({ $push: { following: userId } });
-        }
-        else {
+        } else {
             return res.status(400).json({
                 message: `User already followed to ${user.name}`
             })
-        }
+        };
         return res.status(200).json({
-            message: `${loggedInUser.name} just followed ${user.name}`,
+            message: `${loggedInUser.name} just follow to ${user.name}`,
             success: true
         })
     } catch (error) {
         console.log(error);
     }
 }
-
 export const unfollow = async (req, res) => {
     try {
         const loggedInUserId = req.body.id;
         const userId = req.params.id;
-
-        const loggedInUser = await User.findById(loggedInUserId);   //patel
-        const user = await User.findById(userId);   //keshav
+        const loggedInUser = await User.findById(loggedInUserId);//patel
+        const user = await User.findById(userId);//keshav
         if (loggedInUser.following.includes(userId)) {
             await user.updateOne({ $pull: { followers: loggedInUserId } });
             await loggedInUser.updateOne({ $pull: { following: userId } });
@@ -199,7 +179,7 @@ export const unfollow = async (req, res) => {
             })
         };
         return res.status(200).json({
-            message: `${loggedInUser.name} unfollowed ${user.name}`,
+            message: `${loggedInUser.name} unfollow to ${user.name}`,
             success: true
         })
     } catch (error) {
