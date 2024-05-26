a372815328158f3faf6dc72543daa8a705df0794
PRATEEK MISHRA <153118081+PrateekMishra-1@users.noreply.github.com>
2024-05-26 10:51:21 +0530

    Update userRoute.js (I)

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { Login, Register, bookmarks, follow, getMyProfile, getOtherUsers, logout, unfollow } from "../controllers/usercontroller.js";
+import { Login, Register, bookmark, follow, getMyProfile, getOtherUsers, logout, unfollow } from "../controllers/usercontroller.js";
 import isAuthenticated from "../config/auth.js";
 
 const router = express.Router();
@@ -7,7 +7,7 @@ const router = express.Router();
 router.route("/register").post(Register);
 router.route("/login").post(Login);
 router.route("/logout").get(logout);
-router.route("/bookmark/:id").put(isAuthenticated , bookmarks)
+router.route("/bookmark/:id").put(isAuthenticated , bookmark)
 router.route("/profile/:id").get(isAuthenticated , getMyProfile)
 router.route("/otheruser/:id").get(isAuthenticated , getOtherUsers)
 router.route("/follow/:id").post(isAuthenticated , follow)
